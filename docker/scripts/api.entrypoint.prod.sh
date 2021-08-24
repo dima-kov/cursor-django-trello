c347475fde0728410e8624fb40ca5575eb451bd6
Dima Kovalchuk <dima.kovalchuk@preply.com>
2021-08-24 20:50:14 +0300

    Add migrations run

--- docker/scripts/api.entrypoint.prod.sh
+++ docker/scripts/api.entrypoint.prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd trello/src && gunicorn config.wsgi:application -b 0.0.0.0:8000 || { echo 'runserver failed' ; exit 1; }
+cd trello/src && python manage.py migrate && gunicorn config.wsgi:application -b 0.0.0.0:8000 || { echo 'runserver failed' ; exit 1; }
